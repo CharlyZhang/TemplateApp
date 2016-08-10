ede39b0d1e251a573ddcb97c04bb74cf24b52767
CharlyZhang <chicboi@hotmail.com>
2016-08-10 16:47:18 +0800

    set up a new single view project

---
+++ TemplateApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemplateApp
+//
+//  Created by CharlyZhang on 16/8/10.
+//  Copyright © 2016年 CharlyZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
